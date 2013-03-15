276b4b32ae52e04b53b2e2c64d8775205ff6f1ba
Nuno <Nuno@Nuno-SLT>
2013-03-15 23:27:50 +0000

    Criaçao de codigo para classes: Despesa DespesaRepositorio RegDespUI

--- TesteDespesas/src/testedespesas/Despesa.java
+++ TesteDespesas/src/testedespesas/Despesa.java
@@ -1,14 +1,56 @@
 /*
  * To change this template, choose Tools | Templates
-  * and open the template in the editor.
-   */
-   package testedespesas;
-   
-   /**
-    *
-     * @author Nuno
-      */
-      public class Despesa {
-              
-      }
-      
+ * and open the template in the editor.
+ */
+package testedespesas;
+
+/**
+ *
+ * @author Nuno
+ */
+public class Despesa {
+    
+    private int valor;
+    private String descricao;
+
+    public Despesa() {
+    }
+
+    public Despesa(int valor, String descricao) {
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    
+    /**
+     * @return the valor
+     */
+    public int getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+    
+}
