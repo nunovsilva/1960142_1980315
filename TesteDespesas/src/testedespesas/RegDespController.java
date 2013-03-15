5fe12818e31e4fd2911be8a7fb211bf09dadfb3b
i980315 <i980315@10.8.49.125>
2013-03-15 23:31:17 +0000

    1ª gravação

--- TesteDespesas/src/testedespesas/RegDespController.java
+++ TesteDespesas/src/testedespesas/RegDespController.java
@@ -10,4 +10,43 @@ package testedespesas;
  */
 public class RegDespController {
     
+    private int valor;
+    private String descricao;
+
+    public RegDespController() {
+    }
+
+    public RegDespController(int valor, String descricao) {
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    
+    public int getValor() {
+        return valor;
+    }
+
+    
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+   
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+    public String toString() {
+        return super.toString();
+    }
+    
+    
+    
+    
 }
